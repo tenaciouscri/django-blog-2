b34c8f6137d41ab01cf87011f9a48860a0716dce
tenaciouscri <cridelve@gmail.com>
2022-02-25 14:29:49 +0100

    Added description for context processor

--- blog/context_processors.py
+++ blog/context_processors.py
@@ -2,5 +2,8 @@ from .models import Category
 
 
 def menu_links(request):
+    '''
+    This will show the categories in the dropdown menu in all pages.
+    '''
     links = Category.objects.all()
     return {"links": links}
