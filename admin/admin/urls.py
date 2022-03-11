ca943923c19ff7e40d003e60312642fa2cac3ff3
tenaciouscri <cridelve@gmail.com>
2022-03-11 14:36:32 +0100

    Customise site header, title and index title

--- admin/admin/urls.py
+++ admin/admin/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 
 admin.site.site_header = 'The Admin Lair' #  Change header of admin panel
 admin.site.site_title = 'The Admin Lair' #  Change title of admin panel
+admin.site.index_title = 'The Admin Lair administration' # Customise 'Site administration'
 
 urlpatterns = [
     path('admin/', admin.site.urls),
