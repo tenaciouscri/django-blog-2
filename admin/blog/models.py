d3a0b6b8b6dcd2fc2f811e8098d4f7c0ef5f96e3
tenaciouscri <cridelve@gmail.com>
2022-03-11 14:36:54 +0100

    Showing object title instead of default name

--- admin/blog/models.py
+++ admin/blog/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Blog(models.Model):
+
+    title = models.CharField(max_length=250)
+    body = models.TextField()
+    date_created = models.DateTimeField(auto_now_add=True)
+    last_modified = models.DateTimeField(auto_now=True)
+    is_draft = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.title #  Show title instead of default name
