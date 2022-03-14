fa8f348866f0e293d09d7489aad7969fc4325ca7
tenaciouscri <cridelve@gmail.com>
2022-03-14 08:51:35 +0100

    Customizing the way objects get displayed + adding filters

--- admin/blog/admin.py
+++ admin/blog/admin.py
@@ -3,5 +3,16 @@ from django.contrib import admin
 from blog.models import Blog
 
 
+class BlogAdmin(admin.ModelAdmin):
 
-admin.site.register(Blog) #  Registering model to admin panel
+    list_display = (
+        "title",
+        "date_created",
+        "last_modified",
+        "is_draft",
+    )  # Customising the way object models get displayed
+
+    list_filter = ("is_draft",)  # Adding filter as sidebar
+
+
+admin.site.register(Blog, BlogAdmin)  #  Registering model to admin panel
